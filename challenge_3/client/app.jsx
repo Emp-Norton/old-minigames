292bf8a126bd23a87811240025f9d816b19c99c8
emp-norton <jamesjelenko@gmail.com>
2019-01-08 13:58:53 -0800

    initial commit - challenge 3 - needs major refactor

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,80 @@
+class App extends React.Component {
+	constructor(props){
+		super(props);
+		this.state = {
+			checkingOut: false
+		};
+		this.toggleCheckout = this.toggleCheckout.bind(this);
+		this.updateName = this.updateName.bind(this);
+		this.updateEmail = this.updateEmail.bind(this);
+		this.updatePassword = this.updatePassword.bind(this);
+		this.submitData = this.submitData.bind(this);
+	}
+
+	toggleCheckout() {
+		this.setState({checkingOut: true})
+	}
+
+	updateName(event) {
+		this.setState({name: event.target.value})
+	}
+	updateEmail(event) {
+		this.setState({email: event.target.value})
+	}
+	updatePassword(event) {
+		this.setState({password: event.target.value})
+	}
+
+	submitData() {
+
+		var data = {name: this.state.name, email: this.state.email, password: this.state.password};
+		console.log('click ' + data)
+		fetch('http://localhost:3000/submit', {
+			method: "POST",
+			headers: {
+      	'Content-Type': 'application/json'
+    	},
+			body: JSON.stringify(data)
+		}).then(function(response) {
+			console.log('sent ' + data)
+		})
+	}
 
 
+	render() {
+		return (
+			<div>
+				<NavBar/>
+				<h1> Your current cart </h1> 
+				{ !this.state.checkingOut 
+					? <button onClick={this.toggleCheckout}> Checkout </button>
+					: <FirstForm updateName={this.updateName} updateEmail={this.updateEmail} updatePassword={this.updatePassword} submitData={this.submitData}/>
+					}
+			</div>
+		)
+	}
+}
+
+const NavBar = () => {
+	return (
+		<div className="navbar">
+			Stuff goes here
+		</div>
+		)
+}
+
+const FirstForm = (props) => {
+		return (
+			<form>
+				<input id="name" type="text" placeholder="name" onChange={props.updateName}></input>
+				<input id="email" type="text" placeholder="email address" onChange={props.updateEmail}></input>
+				<input id="password" type="password" onChange={props.updatePassword}></input>
+				<button type="submit" onClick={ (e) => {
+					e.preventDefault();
+					props.submitData();	
+				}}>Next </button>
+			</form>
+		)
+}
+
+ReactDOM.render(<App />, document.getElementById('app'))
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,36 @@
+const express = require('express');
+// const mongodb = require('mongodb');
+const bodyParser = require('body-parser');
+let MongoClient = require('mongodb').MongoClient;
+var db;
 
+
+MongoClient.connect("mongodb://localhost:27017/checkout", function(err, data){
+	if (err) console.log(err);
+	db = data;
+	console.log('connected to mongo');
+});
+
+
+const app = express();
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '/public'));
+
+	
+app.post('/submit', (req, res) => {
+	db.collection('users').insertOne(req.body);
+});
+
+app.get('/users', (req, res) => {
+  db.collection('users').find({}, function(err, data){
+		if (err) console.log(err);
+		data.toArray(function(err, results){
+			if (err) console.log(err);
+			res.json(results);
+		})
+	});
+
+})
+
+app.listen(3000, () => console.log('listening on 3000'));
 
