7d18bb16a98305f9e7d2c3227cebb83ae2cebf06
Emp-Norton <jamesjelenko@gmail.com>
2019-01-28 13:03:14 -0800

    refactor server to serve static files from dist

--- web_app/server.js
+++ web_app/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = 3123;
 
+app.use(express.static(__dirname + '/client/dist'));
 app.get('/', (req, res) => {
 	res.send('ok');
 })
