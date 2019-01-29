bcaa2b66d49cfb6a46fc469be7a14b69c128e2df
Emp-Norton <jamesjelenko@gmail.com>
2019-01-29 10:27:55 -0800

    add nav buttons

--- web_app/client/src/components/NavBar.jsx
+++ web_app/client/src/components/NavBar.jsx
@@ -2,7 +2,12 @@ import React from 'react';
 const NavBar = (props) => {
 	return (
 		<div class="navbar">
-
+      <h1> 8 Bit Bites! </h1>
+      <div class="navbar--button-well">
+        <button class="navbar--button">Snake</button>
+        <button class="navbar--button">War</button>
+        <button class="navbar--button">Tic-Tac-Toe</button>
+      </div>
 		</div>
 		)
 }
