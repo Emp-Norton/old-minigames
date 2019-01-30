ce5fdc975af012751a73b4aa294dbe3202b82173
Emp-Norton <jamesjelenko@gmail.com>
2019-01-30 12:09:38 -0800

    exploring options to serve html pages as iframes

--- web_app/server.js
+++ web_app/server.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const app = express();
 const PORT = 3123;
+const path = require('path');
+const resources = path.join(__dirname, '/client/src/components/resources/');
 
 app.use(express.static(__dirname + '/client/dist'));
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
 	res.send('Please return to root directory ("https://localhost:3123/").');
+})
+
+app.get('/ttt_templ', (req, res) => {
+  res.sendFile(resources + 'ttt_templ.html')
 })
 
 app.listen(PORT, () => {
