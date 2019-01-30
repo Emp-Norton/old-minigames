3b42ec1d3790666ff13a02bdc53eb40808ba9e37
Emp-Norton <jamesjelenko@gmail.com>
2019-01-30 12:08:44 -0800

    half-implementation of each approach -- needs further work

--- web_app/client/src/components/TicTacToe.jsx
+++ web_app/client/src/components/TicTacToe.jsx
@@ -3,7 +3,12 @@ import Page from './resources/ttt_templ.html';
 const htmlDoc = {__html: Page};
 
 const TicTacToe = (props) => {
-  return (<div dangerouslySetInnerHTML={htmlDoc} />)
+  return (
+    <div>
+      <iframe src="/ttt_templ"></iframe>
+      <div dangerouslySetInnerHTML={htmlDoc} />
+    </div>
+    )
 }
 
 export default TicTacToe;
