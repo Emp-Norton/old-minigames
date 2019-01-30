375388d7a5535da1a366c682109782a4ed22f361
Emp-Norton <jamesjelenko@gmail.com>
2019-01-30 12:50:22 -0800

    converted to stateful component, mvp

--- web_app/client/src/components/TicTacToe.jsx
+++ web_app/client/src/components/TicTacToe.jsx
@@ -1,14 +1,26 @@
 import React from 'react';
 import Page from './resources/ttt_templ.html';
 const htmlDoc = {__html: Page};
+import rules from './resources/ttt_rules.js';
 
-const TicTacToe = (props) => {
-  return (
-    <div>
-      <iframe src="/ttt_templ"></iframe>
-      <div dangerouslySetInnerHTML={htmlDoc} />
-    </div>
+ // starting to look like this should be a generic component that
+ // accepts a filestring for the template to load instead of a 
+ // stateful component for each unique script / game.
+    // how to handle game-specific complex functions?
+export default class TicTacToe extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    rules.init();
+  }
+
+  render() {
+    return (
+      <div>
+        <div dangerouslySetInnerHTML = {htmlDoc} />
+      </div>
     )
+  }
 }
-
-export default TicTacToe;
