1d9f0240c1aae6e3fe8e65bb66f43d325ed33477
Emp-Norton <jamesjelenko@gmail.com>
2019-01-30 12:18:04 -0800

    remove unecessary path

--- web_app/server.js
+++ web_app/server.js
@@ -5,12 +5,9 @@ const path = require('path');
 const resources = path.join(__dirname, '/client/src/components/resources/');
 
 app.use(express.static(__dirname + '/client/dist'));
-app.get('/', (req, res) => {
-	res.send('Please return to root directory ("https://localhost:3123/").');
-})
 
-app.get('/ttt_templ', (req, res) => {
-  res.sendFile(resources + 'ttt_templ.html')
+app.get('/', (req, res) => {
+	res.send(`Please return to root directory ("https://localhost:${PORT}/").`);
 })
 
 app.listen(PORT, () => {
