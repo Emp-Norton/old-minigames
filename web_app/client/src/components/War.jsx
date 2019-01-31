18d564b905a368f9dd7f2b4c1dfe9ea3cca307e8
Emp-Norton <jamesjelenko@gmail.com>
2019-01-31 15:41:15 -0800

    minor refactor

--- web_app/client/src/components/War.jsx
+++ web_app/client/src/components/War.jsx
@@ -6,25 +6,52 @@ export default class War extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      player1: null,
-      player2: null
+      player1: {deck: []},
+      player2: {deck: []},
+      isLoaded: false
     }
   }
 
-  componentDidMount() {
+  startGame() {
     const p1 = rules.player('James');
     const p2 = rules.player('Jon');
     rules.makeDeck().shuffle().deal(p1, p2);
-    this.setState({player1: p1, player2: p2});
+    this.setState({player1: p1, player2: p2, isLoaded: true});
+  }
+
+  showDecks(){
+    const {state} = this;
+    return (
+      <div id="battlefield">
+        <Deck name={state.player1.name} cards={state.player1.deck} />
+        <Deck name={state.player2.name} cards={state.player2.deck} />
+      </div>
+    )
+  }
+
+  showPregame() {
+    return (
+      <p> Please wait... </p>
+    )
+  }
+
+
+
+  componentDidMount() {
+    this.startGame();
   }
 
   render(){
+    const { state } = this;
     return (
       <div>
         <h1> WAR </h1>
-        <div id="battlefield">
-          <Deck cards={[]} />
-        </div>
+        {
+          this.state.isLoaded 
+          ? this.showDecks()
+          : this.showPregame()
+        }
+          
       </div>
     )
   }
