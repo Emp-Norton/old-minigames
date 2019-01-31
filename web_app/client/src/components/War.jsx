b98158ae3c1b658882e016347c9bcff1cee5378f
Emp-Norton <jamesjelenko@gmail.com>
2019-01-31 11:19:42 -0800

    add state properties for players, import rules

--- web_app/client/src/components/War.jsx
+++ web_app/client/src/components/War.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
 import Deck from './Deck.jsx';
+import rules from './resources/war_rules.js';
 
 export default class War extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      player1: null,
+      player2: null,
+      p1_deck: [],
+      p2_deck: [],
+    }
+  }
+
+  componentDidMount() {
+    const p1 = rules.player('James');
+    const p2 = rules.player('Jon');
+    this.setState({player1: p1, player2: p2});
   }
 
   render(){
