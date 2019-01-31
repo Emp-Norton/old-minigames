e186c0593fb39de8981a78823fb67406e6e8cdb8
Emp-Norton <jamesjelenko@gmail.com>
2019-01-31 15:45:47 -0800

    add css class to card component

--- web_app/client/src/components/Card.jsx
+++ web_app/client/src/components/Card.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Card = (props) => {
   return (
     <div>
-      <h1> {props.value} </h1>
+      <h1 class="battlefield--card"> {props.value} </h1>
     </div>
   )
 }
