59c457604eb0cd6999ebba0c1b20d66f46a1586d
Emp-Norton <jamesjelenko@gmail.com>
2019-02-05 18:58:06 -0800

    very basic structure for component

--- web_app/client/src/components/DeckPlaceholder.jsx
+++ web_app/client/src/components/DeckPlaceholder.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 export default const DeckPlaceholder = (props) => {
   return (
     <div class="deck--placeholder">
+      <h1> #{ props.card.length } </h1>
     </div>
   )
 }
