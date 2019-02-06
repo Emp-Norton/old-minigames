5d3f98ca82ccbc1a0b19cf211c84a244b9450343
Emp-Norton <jamesjelenko@gmail.com>
2019-02-05 19:01:33 -0800

    refactor to stateful to allow modals on click

--- web_app/client/src/components/DeckPlaceholder.jsx
+++ web_app/client/src/components/DeckPlaceholder.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import Card from './Card.jsx';
 
-export default const DeckPlaceholder = (props) => {
-  return (
-    <div class="deck--placeholder">
-      <h1> #{ props.card.length } </h1>
-    </div>
-  )
+export default class Deck extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cards: [],
+      discarded: []
+    }
+  }
+
+  componentDidMount() {
+    this.setState({cards: this.props.cards})
+  }
+
+  render() {
+    return (
+      <div class="deck--placeholder">
+        <h1> #{ props.card.length } </h1>
+      </div>
+    )
+  }
 }
