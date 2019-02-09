a29c42897482a6e73383d5f13a41bd092e621ad8
Emp-Norton <jamesjelenko@gmail.com>
2019-02-09 12:31:27 -0800

    [old-minigames] removed unnecessary click handler in card component

--- web_app/client/src/components/Card.jsx
+++ web_app/client/src/components/Card.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Card = (props) => {
   return (
     <div>
-      <h1 class="battlefield--card" onClick={(e) => props.clickHandler(props.player)}> {props.value} </h1>
+      <h1 class="battlefield--card"> {props.value} </h1>
     </div>
   )
 }
